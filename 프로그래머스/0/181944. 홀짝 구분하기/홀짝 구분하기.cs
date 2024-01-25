16486551adeb0aff3afe6479a1fc2dd87fd2ccc0
윤혜주 <142863284+unaexoo@users.noreply.github.com>
2024-01-25 15:13:48 +0900

    [level 0] Title: 홀짝 구분하기, Time: 15.91 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        
+        int n = 0;
+        n= int.Parse(Console.ReadLine());
+        if(n%2==0)
+            Console.WriteLine("{0} is even",n);
+        else
+            Console.WriteLine("{0} is odd", n);
+    }
+}
