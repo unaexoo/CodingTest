eeb5d4457ef4e93496cace757723dd34404beaef
윤혜주 <142863284+unaexoo@users.noreply.github.com>
2024-01-25 15:19:17 +0900

    [level 0] Title: 배열 자르기, Time: 0.17 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = new int[num2-num1+1];
+            int cnt = 0;
+            for(int i=num1; i<=num2; i++)
+            {
+                answer[cnt++]= numbers[i];
+            }
+            return answer;
+    }
+}
