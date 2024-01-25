2a6e574b782c2f9c5fdab0a2eba6ddcffa896131
윤혜주 <142863284+unaexoo@users.noreply.github.com>
2024-01-25 15:20:02 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.15 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string, string letter) {
+         string answer = answer = my_string.Replace(letter, "");
+        return answer;
+    }
+}
