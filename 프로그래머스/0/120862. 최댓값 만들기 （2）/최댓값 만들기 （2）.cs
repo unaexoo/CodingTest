b0406c3e36f5dbdc45d7c997f7cfebe093ecc6fe
윤혜주 <142863284+unaexoo@users.noreply.github.com>
2024-01-25 15:17:26 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.18 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Solution {
+    public int solution(int[] numbers) {
+        int answer = int.MinValue;
+        for (int i = 0; i < numbers.Length-1; i++)
+        {
+            for (int j = i+1; j < numbers.Length; j++)
+            {
+                if (numbers[i] * numbers[j] > answer)
+                {
+                    answer = numbers[i] * numbers[j];
+                }
+            }
+        }
+        return answer;
+    }
+}
