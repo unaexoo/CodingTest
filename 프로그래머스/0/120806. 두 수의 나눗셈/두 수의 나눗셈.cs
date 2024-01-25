64c2862c901fd69f4b2cd3cca34336a995f9ec1f
윤혜주 <142863284+unaexoo@users.noreply.github.com>
2024-01-25 15:23:27 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.16 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        double answer =(double) num1/num2;
+        answer*=1000;
+        return (int)answer;
+    }
+}
