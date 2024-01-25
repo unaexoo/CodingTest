64e172b2e491d5767d2344728decb96056a4ab49
윤혜주 <142863284+unaexoo@users.noreply.github.com>
2024-01-25 14:43:16 +0900

    [Bronze V] Title: ??!, Time: 64 ms, Memory: 5380 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,9 @@
+class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            name = name +"??!";
+            Console.WriteLine(name);
+        }
+    }
