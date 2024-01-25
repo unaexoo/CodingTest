21eb4d36e91d7241387773510051597784973402
윤혜주 <142863284+unaexoo@users.noreply.github.com>
2024-01-25 15:14:32 +0900

    [level 0] Title: flag에 따라 다른 값 반환하기, Time: 0.23 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b, bool flag) {
+        int answer = 0;
+        if(flag){
+            return answer=a+b;
+        }else{
+            return answer=a-b;
+        }
+    }
+}
