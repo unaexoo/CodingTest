09f6a33ed36ca41aca964e8aeb18084f241bf380
윤혜주 <142863284+unaexoo@users.noreply.github.com>
2024-11-19 17:38:10 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -0,0 +1,13 @@
+def solution(num_list):
+    answer = 0
+    even, odd = 0,0
+    for i in range(len(num_list)) :
+        if i%2==0:
+            even += num_list[i]
+        else:
+            odd += num_list[i]
+    if even > odd :
+        answer = even
+    else: 
+        answer = odd
+    return answer
